d1e181e061ebb5451542ab30046f9bebea86f47b
samin ahmed <samin.ahmed@northsouth.edu>
2020-10-08 11:54:49 +0600

    CSE-311_Projects

---
+++ doctor/home.php
@@ -0,0 +1,17 @@
+<?php include 'header.php';?>
+</head>
+
+<body>
+
+<?php include 'common_without_login.php';?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<?php include 'slide.php';?>
+
+</body>
+</html>
